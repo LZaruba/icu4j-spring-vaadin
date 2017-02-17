9db0cadfb959c1fe49ff5136f483c875900dc9d8
Lukas Zaruba <lukas.zaruba@gmail.com>
2017-02-17 14:12:22 +0100

    Added readme to the demo application and fixed file comments

---
+++ cz.lzaruba.icu4j.spring.vaadin.demo/readme.md
@@ -0,0 +1,5 @@
+# Demo Application for icu4j-spring-vaadin Add-on
+
+This is minimal Spring Boot Application that shows usage of the icu4j-spring-vaadin Add-on.
+
+You can either import it to your favourite IDE and run it from there or use ```$ mvn spring-boot:run```
--- cz.lzaruba.icu4j.spring.vaadin.demo/src/main/java/cz/lzaruba/icu4j/spring/vaadin/demo/MainUI.java
+++ cz.lzaruba.icu4j.spring.vaadin.demo/src/main/java/cz/lzaruba/icu4j/spring/vaadin/demo/MainUI.java
@@ -1,7 +1,17 @@
 /**
- * Copyright (C) 2017 ZARFA s.r.o. - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
+ * Copyright (C) 2017 Lukas Zaruba, lukas.zaruba@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package cz.lzaruba.icu4j.spring.vaadin.demo;
 
@@ -16,7 +26,7 @@ import com.vaadin.ui.UI;
 import cz.lzaruba.icu4j.spring.vaadin.VaadinICUMessageSource;
 
 /**
- * @author Lukas Zaruba, lukas.zaruba@zarfa.cz
+ * @author Lukas Zaruba, lukas.zaruba@gmail.com
  */
 @SuppressWarnings("serial")
 @SpringUI
